4b04985a16a35e14c19adee4338fe7209ec404a0
EliasFredoni <fredoni.elias@gmail.com>
2022-06-09 11:39:13 +0200

    styles

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Head from './components/Head';
+import Firstsection from './components/Firstsection';
+import Footer from './components/Footer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Head />
+      <Firstsection />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,40 @@
+const Footer = () => {
+    return (
+        <footer>
+            <div className="container">
+                <div>
+                    <img className="grid-item" src="img/salat.jpeg" alt="" />
+                    <img className="grid-item" src="img/pizza.jpeg" alt="" />
+                    <img className="grid-item" src="img/frikadellen.jpeg" alt="" />
+                    <img className="grid-item" src="img/glass.jpeg" alt="" />
+                    <div className="text">
+                        <p className="grid-item">Salatos</p>
+                        <p className="grid-item">Pizza</p>
+                        <p className="grid-item">Bällchen</p>
+                        <p className="grid-item">Aqua</p>
+                        <p>hahahaha</p>
+                        <p>huhu</p>
+                        <p>huhu</p>
+                        <p>hahah</p>
+                    </div>
+                    <img className="grid-item" src="img/pizza2.jpeg" alt="" />
+                    <img className="grid-item" src="img/scampi.jpeg" alt="" />
+                    <img className="grid-item" src="img/spagetthi.jpeg" alt="" />
+                    <img className="grid-item" src="img/teller.jpeg" alt="" />
+                    <div className="text">
+                        <p className="grid-item">Turtles Pizza</p>
+                        <p className="grid-item">Tailändisch</p>
+                        <p className="grid-item">spagetthi</p>
+                        <p className="grid-item">Alles auf einem Haufen</p>
+                        <p>hahahaha</p>
+                        <p>huhu</p>
+                        <p>huhu</p>
+                        <p>hahah</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
